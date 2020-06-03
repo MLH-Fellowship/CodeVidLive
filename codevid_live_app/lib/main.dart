d880ab610b21959d848cb93077b2acc248bde8c2
Micky Yun Chan <michan@redhat.com>
2020-06-03 13:11:27 +0800

    add futurebuilder for g_map

--- codevid_live_app/lib/main.dart
+++ codevid_live_app/lib/main.dart
@@ -1,14 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'package:geolocator/geolocator.dart';
+import 'dart:io';
 
 
-Position position;
-void countSeconds(int s) {
-  for (var i = 1; i <= s; i++) {
-    Future.delayed(Duration(seconds: i), () => print(position.toString()));
-  }
-}
+Future<Position> _future;
 
 void main() {
   WidgetsFlutterBinding.ensureInitialized();
@@ -20,43 +16,52 @@ class MyApp extends StatefulWidget {
   _MyAppState createState() => _MyAppState();
 }
 
-class _MyAppState extends State<MyApp>{
+class _MyAppState extends State<MyApp> {
   GoogleMapController mapController;
-  LatLng _center;
-  void _onMapCreated(GoogleMapController controller){
+
+  void _onMapCreated(GoogleMapController controller) {
     mapController = controller;
   }
 
-  Future<void> initPos() async{
-    Geolocator geolocator = Geolocator()..forceAndroidLocationManager = true;
-    GeolocationStatus geolocationStatus  = await geolocator.checkGeolocationPermissionStatus();
-    position = await Geolocator().getCurrentPosition(desiredAccuracy: LocationAccuracy.high);
-    _center = LatLng(position.latitude, position.longitude);
+  Future<Position> initPos() async {
+    Geolocator geolocator = Geolocator()
+      ..forceAndroidLocationManager = true;
+    GeolocationStatus geolocationStatus = await geolocator
+        .checkGeolocationPermissionStatus();
+    Position position = await geolocator.getCurrentPosition(
+        desiredAccuracy: LocationAccuracy.high);
+    return position;
   }
 
   @override
-  void initState(){
+  void initState() {
     super.initState();
-    initPos();
-    countSeconds(4);
+    _future = initPos();
   }
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text('Maps Sample App'),
-          backgroundColor: Colors.green[700],
-        ),
-        body: GoogleMap(
-          onMapCreated: _onMapCreated,
-          initialCameraPosition: CameraPosition(
-            target: _center,
-            zoom: 11.0,
-          ),
-        ),
-      ),
+        home: Scaffold(
+            appBar: AppBar(
+              title: Text("CodeVid_live"),
+            ),
+            body: FutureBuilder(
+                future: _future,
+                builder: (context, snapshot) {
+                  print(snapshot.data.latitude);
+                  return Stack(children: <Widget>[
+                    GoogleMap(
+                      initialCameraPosition: CameraPosition(
+                          target: LatLng(
+                              snapshot.data.latitude, snapshot.data.longitude),
+                          zoom: 12.0),
+                      onMapCreated: _onMapCreated,
+                    ),
+                  ]
+                  );
+                })
+        )
     );
   }
 }
