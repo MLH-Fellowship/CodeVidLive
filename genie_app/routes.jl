a5cac271b7a3a9ba9ecc2a1dae7b91715ceeaab4
Rashika Karki <41114269+RashikaKarki@users.noreply.github.com>
2020-06-05 01:54:15 +0545

    Add API

--- genie_app/routes.jl
+++ genie_app/routes.jl
@@ -3,67 +3,42 @@ import Random
 
 using Genie, Genie.Router
 import Genie.Renderer.Json: json
+using Genie.Requests
+using HTTP
 
-# Probably need to move this logic to separate files
-# Struct for maps data
+using CSV
+
+df = CSV.read("./US_State_Final_data.csv", normalizenames = true)
+
 struct Prediction
+  date
+  state::String
+  totalInfection::Int64
+  dailyInfection::Int64
   longitude::Float64
   latitude::Float64
   chance::Float64
 end
 
-# Struct for visualization
-struct VisualPoint
-  date
-  noOfInfections
-end
-
-# Serializers to json
 function toJson(prediction::Prediction) 
-  Dict(:latitude => prediction.latitude, :longitude => prediction.longitude, :chance => prediction.chance)
+  Dict(:date => prediction.date, :state=> prediction.state, :totalInfection=> prediction.totalInfection, :dailyInfection=> prediction.dailyInfection, :longitude=> prediction.longitude, :latitude=> prediction.latitude, :chance=> prediction.chance)
 end
 
-function toJson(point::VisualPoint)
-  Dict(:date => point.date, :noOfInfections => point.noOfInfections)
-end
+Genie.config.run_as_server = true
 
 route("/") do
   serve_static_file("welcome.html")
 end
 
-route("/api/prediction") do
-  # Get required longitude and latitude query params
-  longitude = haskey(@params, :longitude) ? parse(Float64, @params(:longitude)) : 0.0
-  latitude = haskey(@params, :latitude) ? parse(Float64, @params(:latitude)) : 0.0
-
-  # Validate params exist
-  if longitude == "" || latitude == ""
-    Dict(:message => "Longitude or latitude parameters are missing!") |> json
-  else # If all validation passes
-      # Return dummy data for now
-      prediction = Prediction(longitude, latitude, 0.59)
-      nearby_predictions = [Prediction(longitude, latitude, 0.59), Prediction(longitude, latitude, 0.59), Prediction(longitude, latitude, 0.59)]
-      nearby_predictions_json = map(toJson, nearby_predictions);
-      Dict("requested" => toJson(prediction), "nearby" => nearby_predictions_json) |> json
-  end
+route("/api/prediction",method = POST ) do
+  dummy = 0
+  points = []
+       for i in 1:size(df,1)
+        push!(points, toJson(Prediction(df[i,2], df[i,3],df[i,4],df[i,5],df[i,6],df[i,7],dummy)))
+        
+        end
+       
+  Dict(:points => points) |> json;
 end
 
-route("/api/time/series") do
-  # Get required longitude and latitude query params
-  longitude = haskey(@params, :longitude) ? parse(Float64, @params(:longitude)) : 0.0
-  latitude = haskey(@params, :latitude) ? parse(Float64, @params(:latitude)) : 0.0
-
-  # Validate params exist
-  if longitude == "" || latitude == ""
-    Dict(:message => "Longitude or latitude parameters are missing!") |> json
-  else # If all validation passes
-       # Return 10 points of dummy data for now
-      
-       points = []
-       today = Dates.today()
-       for counter in [0:1:10;]
-        push!(points, toJson(VisualPoint(today, rand(1:100))))
-       end
-       Dict(:points => points) |> json;
-  end
-end
+Genie.startup(async = false)
