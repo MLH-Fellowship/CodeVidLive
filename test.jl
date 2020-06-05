bd89a34b5f2d6a7a01eec8295fea9147c378a761
Zing <zzj0402@gmail.com>
2020-06-05 14:57:34 +1200

    read from json for ETA

--- test.jl
+++ test.jl
@@ -5,4 +5,6 @@ using Test
 @test predict(34.1330949, -117.9143879, 33.8068768, -118.3527671, 3600) < 1
 @test predict(34.1330949, -117.9143879, 33.8068768, -118.3527671, 3600) >= 0
 
+@test read_response() isa Dict
+
 println("Test Successful")
