0453c9ea5e3f650a547f630b67dc306ec0d4f5d7
Micky Yun Chan <michan@redhat.com>
2020-06-05 11:43:00 +0800

    add return eta and remove API key

--- direction.jl
+++ direction.jl
@@ -1,10 +1,14 @@
 using HTTP
 using JSON
-function toJson(longitude::Float64, latitude::Float64, longitude2::Float64, latitude2::Float64) 
-  r = HTTP.request("GET", "https://api.mapbox.com/directions/v5/mapbox/driving/$longitude%2C$latitude%3B$longitude2%2C$latitude2?alternatives=false&geometries=geojson&steps=false&access_token=pk.eyJ1IjoiZGlhZ2EiLCJhIjoiY2them1jMGY1MGI1YjJ6bG8yZHlnd2trNSJ9.FSly4QvdXoFjVlqXldieew")
+using Dates
+
+function toJson(longitude::Float64, latitude::Float64, longitude2::Float64, latitude2::Float64, _time::DateTime) 
+  r = HTTP.request("GET", "https://api.mapbox.com/directions/v5/mapbox/driving/$longitude%2C$latitude%3B$longitude2%2C$latitude2?alternatives=false&geometries=geojson&steps=false&access_token=")
   j = JSON.parse(String(r.body))
-  print(j["routes"][1]["legs"][1]["duration"])
   open("./data/foo.json","a") do f 
     JSON.print(f, j)
   end 
+  eta = _time + Dates.Second(Int(round(j["routes"][1]["legs"][1]["duration"])))
+  print(Dates.format(eta, "HH:MM:SS"))
+  return eta
 end
